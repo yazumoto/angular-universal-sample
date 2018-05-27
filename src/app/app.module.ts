21ccc793217be20531eea54e39d099f9c5233edd
Zenn <app.zenn@gmail.com>
2018-05-27 15:20:07 +0900

    feat (app.module): update (https://angular.io/guide/universal#the-root-appmodule)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {APP_ID, Inject, NgModule, PLATFORM_ID} from "@angular/core";
 
 import { AppComponent } from './app.component';
+import {isPlatformBrowser} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'rezadden-front' }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string
+  ) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
